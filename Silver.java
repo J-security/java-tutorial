c633399df3254673290a2280515be4968b3580cb
omage1 <46792811+omage1@users.noreply.github.com>
2019-01-17 23:52:42 -0800

    Add files via upload

---
+++ Silver.java
@@ -0,0 +1,18 @@
+package silver;
+
+public class Silver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Function1(6.5f, 7.6f);
+	
+	}
+	
+	public static void Function1(float var1, float var2)
+	{
+		System.out.println(var1 * var2);
+	}
+	
+
+}
